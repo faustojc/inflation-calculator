dbf2a1f3fe7b5ad74659ca29ee681f11094d94d4
faustojc <faustojcboko@gmail.com>
2026-01-27 11:21:56 +0800

    chore: removed ncr in data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ export default function App() {
 				}
 			}
 
-			const uniqueKeys = new Set([hierarchy.target.key, hierarchy.province?.key, hierarchy.region?.key, hierarchy.national?.key, "ncr"]);
+			const uniqueKeys = new Set([hierarchy.target.key, hierarchy.province?.key, hierarchy.region?.key, hierarchy.national?.key]);
 			const keysToFetch = Array.from(uniqueKeys).filter(Boolean) as string[];
 			const batchMap = await getCalculationData(keysToFetch, dates.startYear - 1, dates.endYear);
 			const result = calculatePersonalInflation(items, { hierarchy }, dates, { mode: currentMode, totalInput: currentTotalAlloc }, batchMap);
