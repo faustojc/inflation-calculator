1f2fb577e9665407dffc7d72941d7af7d07d55ed
faustojc <faustojcboko@gmail.com>
2026-02-25 13:06:54 +0800

    chore: added the capita

--- src/components/controls/IncomeClassControl.tsx
+++ src/components/controls/IncomeClassControl.tsx
@@ -6,21 +6,27 @@ const IncomeClassControl = () => {
 	const appSettings = useStore(settings);
 
 	return (
-		<div>
+		<div className="space-y-1">
 			<Select value={appSettings.incomeClass} onValueChange={(val) => settings.setKey("incomeClass", val as IncomeClass)}>
 				<SelectTrigger className="w-full">
 					<SelectValue />
 				</SelectTrigger>
 				<SelectContent>
 					<SelectItem value="ALL">All Income Households</SelectItem>
-					<SelectItem value="B30">Bottom 30% Income (as of 2018)</SelectItem>
+					<SelectItem value="B30">Bottom 30% Income</SelectItem>
 				</SelectContent>
 			</Select>
-			{/* {appSettings.area.capita && appSettings.incomeClass === "B30" && (
-				<span className="text-xs text-foreground">
-					Capita: {Intl.NumberFormat("en-US", { style: "currency", currency: "PHP", maximumFractionDigits: 2 }).format(appSettings.area.capita)}
+			{appSettings.area.capita && appSettings.incomeClass === "B30" && (
+				<span className="text-xs text-foreground flex items-center gap-1">
+					<span>Capita:</span>
+					<span className="font-bold">
+						{Intl.NumberFormat("en-US", { style: "currency", currency: "PHP", maximumFractionDigits: 2 }).format(
+							appSettings.area.capita,
+						)}
+					</span>
+					<span>(as of 2018)</span>
 				</span>
-			)} */}
+			)}
 		</div>
 	);
 };
