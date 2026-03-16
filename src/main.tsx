2180e63a6f6740acc3729c0e645ecd4c7f7be40a
faustojc <faustojcboko@gmail.com>
2026-03-16 15:02:28 +0800

    Remove syncfusion

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { registerLicense } from "@syncfusion/ej2-base";
 import App from "@/App.tsx";
-
 import "./index.css";
-import "@syncfusion/ej2-base/styles/material.css";
-
-const syncfusionLicenseKey = import.meta.env.VITE_SYNCFUSION_LICENSE_KEY;
-if (syncfusionLicenseKey) {
-	registerLicense(syncfusionLicenseKey);
-} else {
-	console.warn("Syncfusion license key is missing in environment variables.");
-}
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
