ebc71f3ea64199061db254b0c95537f792a03350
faustojc <faustojcboko@gmail.com>
2026-03-18 08:31:54 +0800

    chore: accept the trade off, dasig nmn

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, type Plugin } from "vite";
-
-const deferDepsPlugin = (): Plugin => ({
-	name: "defer-css-and-js",
-	enforce: "post",
-	transformIndexHtml(html) {
-		let modifiedHtml = html.replace(
-			/<link rel="stylesheet" crossorigin href="([^"]+)">/g,
-			'<link rel="preload" as="style" href="$1" />\n\t\t<link rel="stylesheet" crossorigin href="$1" media="print" onload="this.media=\'all\'" />\n\t\t<noscript><link rel="stylesheet" crossorigin href="$1" /></noscript>',
-		);
-
-		modifiedHtml = modifiedHtml.replace(
-			/<script type="module" crossorigin src="([^"]+)"><\/script>/g,
-			'<link rel="modulepreload" crossorigin href="$1" />\n\t\t<script type="module" crossorigin src="$1"></script>',
-		);
-
-		return modifiedHtml;
-	},
-});
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss(), deferDepsPlugin()],
+	plugins: [react(), tailwindcss()],
 	base: "/",
 	build: {
 		target: "esnext",
